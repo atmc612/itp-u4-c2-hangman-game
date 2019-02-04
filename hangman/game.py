a24c09c74e5c325bf8ea37323c59049dd4444985
atmc612 <atmc612@gmail.com>
2019-02-04 06:30:30 +0000

    all tests passed, ready to merge

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,68 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
 LIST_OF_WORDS = []
 
 
 def _get_random_word(list_of_words):
-    pass
+    if not list_of_words:
+        raise InvalidListOfWordsException
+    
+    return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException
+        
+    masked_word = ''
+    for i in range(len(word)):
+        masked_word += '*'
+    return masked_word
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
 
+    if not answer_word or not masked_word:
+        raise InvalidWordException
+    if len(character) > 1:
+        raise InvalidGuessedLetterException
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException
+        
+    result = list(masked_word)
+    
+    try:
+        for index, char in enumerate(list(answer_word)):
+            if char.lower() == character.lower():
+                result[index] = character.lower()
+        result = ''.join(result)
+        return result
+    except:
+        return result
+    
 
-def guess_letter(game, letter):
-    pass
+def guess_letter(game, letter):   
+    if game['answer_word'] == game['masked_word'] or game['remaining_misses'] == 0 :
+        raise GameFinishedException
+    
+    uncovered_word = _uncover_word(game['answer_word'],game['masked_word'],letter.lower())
+    
+    if game['masked_word'] == uncovered_word:
+        game['remaining_misses'] -= 1
+    game['masked_word'] = uncovered_word
+    game['previous_guesses'].append(letter.lower())
+    
+
+    if game['answer_word'] == game['masked_word']:
+        game_over = True
+        raise GameWonException('You win!')
+        
+    if game['remaining_misses'] == 0:
+        game_over = True
+        raise GameLostException('You lost!')
+    
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
